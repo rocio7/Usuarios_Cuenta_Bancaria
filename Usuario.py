2515ee7a7936d2439d6cec4886061dd5793864bf
rocio <rocioma324@gmail.com>
2022-03-08 00:30:34 -0500

    segundo cambio

--- Usuario.py
+++ Usuario.py
@@ -13,11 +13,7 @@ class User:
         print(f"Usuario: {self.nombre}, Balance Cuenta1: {self.cuenta['cuenta1'].mostrar_info_cuenta()}")
         print(f"Usuario: {self.nombre}, Balance Cuenta2: {self.cuenta['cuenta2'].mostrar_info_cuenta()}")
 
-    def transfer_dinero(self, otro_usuario, cantidad):
-        self.cuenta -= cantidad
-        otro_usuario.cuenta += cantidad
-
-    def transfer_money(self,cantidad,user):
+    def transferencia_dinero(self,cantidad,user):
         self.cuenta -= cantidad
         user.cuenta += cantidad
         self.mostrar_balance_usuario()
